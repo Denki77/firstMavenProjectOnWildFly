de0bab742f5cab3e0d5f49b6f22966ff4cc43b31
Denis_Fomenkov <tgimrv@ya.ru>
2021-03-01 00:04:12 +0300

    first step

---
+++ src/main/java/ru/reborsa/reborsa/ReborsaApplication.java
@@ -0,0 +1,13 @@
+package ru.reborsa.reborsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReborsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReborsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/reborsa/reborsa/ReborsaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.reborsa.reborsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReborsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
